c07192145d9d998f764d623b4adea336528e2a63
Jatin Yadav <jatin.yadav@timesinternet.in>
2022-03-10 13:50:49 +0530

    added display changes

---
+++ assets/style.js
@@ -0,0 +1,30 @@
+
+function advance_button_function(){
+    var div = document.getElementById('advance_settings');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+}
+
+function sizemapping_button_function(){
+    var div = document.getElementById('size_mapping');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+}
+
+function refresh_button_function(){
+    var div = document.getElementById('refresh_settings');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+}
